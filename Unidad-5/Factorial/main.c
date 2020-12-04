e8a0269f8b5ca31d0aae374bc394dc8dbdf95651
Cuda98 <kudah98@gmail.com>
2020-12-04 17:20:43 -0300

    Practicas

---
+++ Unidad-5/Factorial/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// método recursivo que calcule la suma de un arreglo de numeros enteros
+
+int main()
+{
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+ int factorialR(int n) // Supongamos que entra un 5
+ {
+    if(n == 1) // cuando n vale 1, devuelve. (esto sería el caso de corte-base), recordar que si no ponemos esto, la función no tiene límite.
+    {
+        return 1;
+    }
+
+    return n*factorialR(n-1);  //El retorno va a ser 5 * (Volver a llamar a la función) 5-1= 4
+     // 5 * el numero que entró al principio - 1, 4* 4-1,
+ }
+
+void mostrarHasta(int n)
+ {  caso base  y se va a repetir hasta que el número sea un 0.
+     if(n == 0)
+    {
+        return 0;
+    }
+
+    //caso general
+    printf("%d, ", n); // muestra el 8 ........ muestra el 7
+    mostrarHasta(n-1); // vuelve a entrar con 8-1 ........... vuelve a entrar con 7-1
+    // Si ponés el printf acá, va de 1 2 3 4 5 6 7 8
+
+ }
+*/
